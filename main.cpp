70a771f3a55c8dfb22303113f54a8a4b5fdd7c93
Khayet <jakob.herpel@uni-weimar.de>
2015-11-12 00:58:54 +0100

    Implement is_palindrome_queue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "stack.hpp"
+#include "queue.hpp"
 
 bool argument_errors(int num_args) {
   if (num_args < 2) {
@@ -23,6 +24,8 @@ bool is_palindrome_stack(std::string const& str) {
     my_stack.push(i);
   }
 
+  if (str.size() != my_stack.size()) return false;
+
   for (auto i : str) {
     if (i == my_stack.top()) {
       my_stack.pop();
@@ -31,10 +34,31 @@ bool is_palindrome_stack(std::string const& str) {
     }
   }
 
-  return true;
+  if (my_stack.empty()) {
+    return true;
+  }
+
+  return false;
 }
 
 bool is_palindrome_queue(std::string const& str) {
+  Queue<char> my_queue;
+
+  for (auto i : str) {
+    my_queue.push(i);
+  }
+
+  if (str.size() != my_queue.size()) return false;
+
+  for (auto i : str) {
+    if (i != my_queue.back()) return false;
+    my_queue.pop();
+  }
+
+  if (my_queue.empty()) {
+    return true;
+  }
+
   return false;
 }
 
@@ -43,6 +67,8 @@ int main(int argc, char* argv[]) {
 
   std::string str = argv[1];
 
+  std::cout << std::endl;
+
   std::cout << "Computed with a Stack: " << std::endl;
   if (is_palindrome_stack(str)) {
     std::cout << str << " is a palindrome" << std::endl;
@@ -50,6 +76,16 @@ int main(int argc, char* argv[]) {
     std::cout << str << " is NOT a palindrome" << std::endl;
   }
 
+  std::cout << std::endl;
+
+  std::cout << "Computed with a Queue: " << std::endl;
+  if (is_palindrome_stack(str)) {
+    std::cout << str << " is a palindrome" << std::endl;
+  } else {
+    std::cout << str << " is NOT a palindrome" << std::endl;
+  }
+
+  std::cout << std::endl;
 
   return 0;
 }
